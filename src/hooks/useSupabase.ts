723498639b63bdbe41ff0dfbee353216e1afda27
Damien Schneider <damien.schneider01@gmail.com>
2023-08-04 21:46:35 +0200

    :truck: Move client supabase to a custom hook

--- src/hooks/useSupabase.ts
+++ src/hooks/useSupabase.ts
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 import { Database } from '@lib/supabase.types';
 
 const supabase = createClient<Database>(
-  import.meta.env.SUPABASE_URL,
-  import.meta.env.SUPABASE_ANON_KEY
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
 );
 export default supabase;
