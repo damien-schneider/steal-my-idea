df67cf9f420239a8dd0a474e71809be0444b0fce
Damien Schneider <damien.schneider01@gmail.com>
2023-08-04 23:22:03 +0200

    :sparkles: Add infinite pannel

---
+++ src/Map.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch';
+
+export default function Map() {
+  return (
+    <div className="w-screen h-screen bg-pink-300">
+      <div className="w-full overflow-hidden bg-blue-300 h-3/4">
+        <TransformWrapper
+          initialScale={1}
+          initialPositionX={200}
+          initialPositionY={100}
+          limitToBounds={false}
+          disablePadding={true}
+          minScale={0.05}
+        >
+          <TransformComponent wrapperClass="w-full h-full">
+            <div>
+              {/* Insérez ici les éléments que vous voulez sur la carte. */}
+              <div className="w-96 h-96 bg-slate-500">
+                Mehuiazhijukerhazeihriuezaghuirghrehuirghuihzejhgtrfiuhezuirhgeziuhiu
+              </div>
+            </div>
+          </TransformComponent>
+        </TransformWrapper>
+      </div>
+    </div>
+  );
+}
