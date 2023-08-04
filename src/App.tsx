cd0b87a4333cf302d2e55c2e7f7bd15079f092a6
Damien Schneider <damien.schneider01@gmail.com>
2023-08-04 21:47:40 +0200

    :sparkles: Retrieve every ideas

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import AddIdea from './AddIdea';
+import IdeaMap from './IdeaMap';
 import './index.css';
 function App() {
   return (
     <>
+      <h1>Steal my Idea</h1>
       <AddIdea />
+      <IdeaMap />
     </>
   );
 }
