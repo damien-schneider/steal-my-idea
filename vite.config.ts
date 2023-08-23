41b95275e87b4c71fab786de3e788c283c5e9c98
Romariin <romainrogier2@gmail.com>
2023-08-23 19:41:57 +0200

    🚚 Remove src path (replace by @/)

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-      src: path.resolve(__dirname, './src'),
       '@lib': path.resolve(__dirname, './src/lib'),
       '@components': path.resolve(__dirname, './src/components'),
       '@hooks': path.resolve(__dirname, './src/hooks')
