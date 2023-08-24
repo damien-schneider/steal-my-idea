8dee2059db57812c9e4f473d3d3ea491748a5539
Antoine Jésus <antoine.jesus@telecomnancy.net>
2023-08-24 20:16:47 +0200

    :seedling: Populate ideas table

---
+++ supabase/seed.sql
@@ -0,0 +1,7 @@
+insert into
+  public.ideas (created_at, estimated_time, title, description, category)
+values
+  ('2021-08-24T15:57:39.000000+00:00', '1 journée', 'Application pour gérer sa todo list', 'Créer une application avec React Native et Supabase pour gérer sa todolist pro et perso au quotidien.', '{"informatique","webdesign","graphisme","design produit"}'),
+  ('2021-08-24T15:57:39.000000+00:00', '2 semaines', 'Club de deltaplane de salon', 'Fonder une club local de passionnés de deltaplane de salon pour pouvoir pratiquer sur une base régulière.', '{"sport","entrepreunariat"}'),
+  ('2021-08-24T15:57:39.000000+00:00', '3 ans', 'Sculpture en rouleaux de papier toilette vides', 'Reconstituer le Taj Mahal en échelle 1:2 avec des rouleaux de papier toilette vides.', '{"artistique","bricolage"}')
+;
